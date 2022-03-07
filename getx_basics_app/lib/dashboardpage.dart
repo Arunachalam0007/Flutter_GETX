528c8ebc9e63614a5f88e955abf6668f2c80988a
Arunachalam0007 <arunsha0007@gmail.com>
2022-03-07 21:16:33 +0530

    OBX with Custom Class value Reactive.

--- getx_basics_app/lib/dashboardpage.dart
+++ getx_basics_app/lib/dashboardpage.dart
@@ -4,8 +4,10 @@ import 'package:getx_basics_app/dashboard_getx_provider.dart';
 import 'package:getx_basics_app/usermodel.dart';
 
 class DashboardPage extends StatelessWidget {
+  Rx<UserData> userData = UserData(name: 'sha', gender: 'male', age: 20).obs;
+
   // // GETX Controller
-  // var dashboardData =
+  // DashboardData dashboardData =
   //     Get.put(DashboardData(dashboardName: 'Developer', dashboardNumber: 3));
 
   @override
@@ -17,68 +19,66 @@ class DashboardPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          GetX<DashboardData>(
-              init: DashboardData(),
-              builder: (data) {
-                return Column(
-                  children: [
-                    Text(
-                      'Dashboard: ${data.dashboardName}',
-                      style: TextStyle(color: Colors.red, fontSize: 30),
-                    ),
-                    Text(
-                      'Dashboard Serial Number: ${data.dashboardNumber}',
-                      style: TextStyle(color: Colors.red, fontSize: 20),
-                    ),
-                  ],
-                );
-              }),
-          // Obx(() => Column(
-          //       crossAxisAlignment: CrossAxisAlignment.start,
-          //       children: [
-          //         Text('User Name: ${userData.value.name}'),
-          //         Text('User Age: ${userData.value.age}'),
-          //         Text('User Gender: ${userData.value.gender}'),
-          //       ],
-          //     )),
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   children: [
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         userData.update((userDataVal) {
-          //           userDataVal?.age--;
-          //         });
-          //       },
-          //       child: const Icon(Icons.remove),
-          //     ),
-          //     const SizedBox(
-          //       width: 10,
-          //     ),
-          //     Obx(
-          //       () => Column(
-          //         children: [Text('${userData.value.age}')],
+          // GetX<DashboardData>(builder: (data) {
+          //   return Column(
+          //     children: [
+          //       Text(
+          //         'Dashboard: ${data.dashboardName}',
+          //         style: TextStyle(color: Colors.red, fontSize: 30),
           //       ),
-          //     ),
-          //     const SizedBox(
-          //       width: 10,
-          //     ),
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         userData.update((userDataVal) {
-          //           userDataVal?.age++;
-          //         });
-          //       },
-          //       child: const Icon(Icons.add),
-          //     ),
-          //   ],
-          // ),
-          // ElevatedButton(
-          //   child: Text('Go to Home Page'),
-          //   onPressed: () {
-          //     Get.back();
-          //   },
-          // ),
+          //       Text(
+          //         'Dashboard Serial Number: ${data.dashboardNumber}',
+          //         style: TextStyle(color: Colors.red, fontSize: 30),
+          //       ),
+          //     ],
+          //   );
+          // }),
+          Obx(() => Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('User Name: ${userData.value.name}'),
+                  Text('User Age: ${userData.value.age}'),
+                  Text('User Gender: ${userData.value.gender}'),
+                ],
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  userData.update((userDataVal) {
+                    userDataVal?.age--;
+                  });
+                },
+                child: const Icon(Icons.remove),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Obx(
+                () => Column(
+                  children: [Text('${userData.value.age}')],
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  userData.update((userDataVal) {
+                    userDataVal?.age++;
+                  });
+                },
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            child: Text('Go to Home Page'),
+            onPressed: () {
+              Get.back();
+            },
+          ),
         ],
       ),
     );
